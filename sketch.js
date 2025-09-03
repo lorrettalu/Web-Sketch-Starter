732efcbca03333ccea9bb8441accf46d7e6e543a
lorrettalu <lu10@ucsb.edu>
2025-09-03 14:12:39 -0700

    Added Wednesday Night

--- sketch.js
+++ sketch.js
@@ -414,6 +414,8 @@ function dialogueText() {
     wednesdayDialogue();
   } else if (wednesdayAfternoon) {
     wednesdayDialogue();
+  } else if (wednesdayNight) {
+    wednesdayDialogue();
   } else {
     // Default Dialogue
     change = true;
@@ -745,6 +747,37 @@ function wednesdayDialogue() {
       text("It is Wednesday afternoon and I garden.", 40, 510);
     }
   }
+  if (wednesdayNight) {
+    if (cottageClick || showMenu) {
+      showMenu = true;
+      switch (wed4ChatState) {
+        case 0:
+          text("I can make food or nap at home.", 40, 510);
+          text("Which one should I do?", 40, 540);
+          break;
+        case 1:
+          text("I nap.", 40, 510);
+          break;
+        case 10:
+          text("I make food.", 40, 510);
+          break;
+        case 20:
+          text("Something special happens.", 40, 510);
+          break;
+        default:
+          text("I finish my task.", 40, 510);
+          change = true;
+          event = false;
+          showMenu = false;
+      }
+    } else if (waterClick) {
+      change = true;
+      text("It is Wednesday night and I got water.", 40, 510);
+    } else if (gardenClick) {
+      change = true;
+      text("It is Wednesday night and I garden.", 40, 510);
+    }
+  }
 }
 
 // Floating Cursor Text
@@ -941,6 +974,7 @@ function mousePressed() {
         wedChatState += 0;
         wed2ChatState += 0;
         wed3ChatState += 0;
+        wed4ChatState += 0;
       }
       clickedFood = false;
       
@@ -980,6 +1014,11 @@ function mousePressed() {
       if (wednesdayAfternoon) {
         wed3ChatState += 10;
       }
+
+      // Wednesday Night
+      if (wednesdayNight) {
+        wed4ChatState += 10;
+      }
   
     } else if (clickedFood == false && ((mouseX > menuOptions[1].w + 50 && mouseX < menuOptions[1].x + 78) && (mouseY > menuOptions[1].y - 7 && mouseY < menuOptions[1].y + 20))) {
       handleOption("Nap");
@@ -1004,6 +1043,9 @@ function mousePressed() {
       }
       if (wednesdayAfternoon && wed3ChatState != 10) {
         wed3ChatState++;
+      }
+      if (wednesdayNight) {
+        wed4ChatState++;
       }
     }
   } else {
@@ -1185,6 +1227,19 @@ function clicks() {
       }
     }
   }
+
+  if (wednesdayNight) {
+    if (wed4ChatState == 20) {
+      if ((mouseX > 48 && mouseX < 318) && (mouseY > 97 && mouseY < 197)) {
+        wed4ChatState++;
+        plop.play();
+      }
+      if ((mouseX > 48 && mouseX < 318) && (mouseY > 220 && mouseY < 320)) {
+        wed4ChatState++;
+        plop.play();
+      }
+    }
+  }
   
 
 }
@@ -1355,6 +1410,19 @@ function dialogueChoices() {
       text("Option 2", 120, 260);
     }
   }
+
+  // Wednesday Night Chat Event
+  if (wednesdayNight) {
+    if (wed4ChatState == 20) {
+      fill(200, 220, 255);
+      noStroke();
+      rect(48, 97, 270, 100, 8);
+      rect(48, 220, 270, 100, 8);
+      fill(0);
+      text("Option 1", 120, 150);
+      text("Option 2", 120, 260);
+    }
+  }
   
 
   pop();
@@ -1500,6 +1568,25 @@ function dialogueHover() {
 
   if (wednesdayAfternoon) {
     if (wed3ChatState == 2) {
+      if ((mouseX > 48 && mouseX < 318) && (mouseY > 97 && mouseY < 197)) {
+        fill(80, 100, 255);
+        noStroke();
+        rect(48, 97, 270, 100, 8);
+        fill(0);
+        text("Option 1", 120, 150);
+      }
+      if ((mouseX > 48 && mouseX < 318) && (mouseY > 220 && mouseY < 320)) {
+        fill(80, 100, 255);
+        noStroke();
+        rect(48, 220, 270, 100, 8);
+        fill(0);
+        text("Option 2", 120, 260);
+      }
+    }
+  }
+
+  if (wednesdayNight) {
+    if (wed4ChatState == 20) {
       if ((mouseX > 48 && mouseX < 318) && (mouseY > 97 && mouseY < 197)) {
         fill(80, 100, 255);
         noStroke();
